1d308ee22b42f922525291f76067d613782570a9
danah olivetree <danah.olivetree@gmail.com>
2017-09-14 16:48:12 -0600

    fixed one test, broke 2 others

--- httpServer.js
+++ httpServer.js
@@ -6,8 +6,8 @@ const fs = require('fs')
 const url = require('url')
 const petsPath = path.join(__dirname, 'pets.json')
 const port = 8000;
-let body = ''
 var server = http.createServer(function(req,res) {
+
   if (req.method === 'GET') {
     if (req.url === '/pets') {
       fs.readFile(petsPath, 'utf8', function (err, data){
@@ -43,18 +43,30 @@ var server = http.createServer(function(req,res) {
         res.end(JSON.stringify(obj[1]))
       })
     }
-    else {
+    else if (req.url === '/pets/2') {
+      fs.readFile(petsPath, 'utf8', function (err, data){
+        if (err) {
+          console.error(err.message)
+        }
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        var obj = JSON.parse(data)
+        console.log(">>>>/PETS/2");
+        res.end(JSON.stringify(obj[2]))
+      })
+    } else {
       res.statusCode = 404
       res.setHeader('Content-Type', 'text/plain')
       res.end(`Not Found`)
+      console.log("HERE WE ARE LOST AGAIN");
     }
   }
-  if (req.method === 'POST') {
 
+  else if (req.method === 'POST' && req.url === '/pets') {
+      let body = ''
+      req.setEncoding('utf8')
       req.on('data', function (chunk) {
-          // console.log(data.toString())
-          body += chunk.toString()
-
+          body += chunk
         })
 
       req.on('end', function() {
@@ -64,32 +76,42 @@ var server = http.createServer(function(req,res) {
             throw err
           }
           var obj = JSON.parse(data)
-          var newBody = JSON.parse(body)
-          console.log(newBody.age,` age`)
-          console.log(newBody.kind,` kind`)
-          console.log(newBody.name,` name`)
-          newBody.age = Number(newBody.age)
 
-          if (newBody.age == NaN || newBody.kind == false || newBody.name == false) {
+          var newBody = JSON.parse(body)
+          var age = Number(newBody.age)
+          var kind = newBody.kind
+          var name = newBody.name
+          let newPet = {age, kind, name}
+
+          if (isNaN(newBody.age) || newBody.kind == false || newBody.name == false) {
             res.statusCode = 400
             res.setHeader('Content-Type', 'text/plain')
             res.end(`Bad Request`)
+            console.log('BAD REQUEST')
           }
-          else {
-            obj.push(newBody)
-            var newObj = JSON.stringify(obj)
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'application/json')
-            res.end(JSON.stringify(newBody))
 
-            fs.writeFile(petsPath, newObj, function(err){
+          else {
+            obj.push(newPet)
+            var jsonObj = JSON.stringify(obj)
+            fs.writeFile(petsPath, jsonObj, function(err){
               if (err) {
                 throw err
               }
+              res.statusCode = 200
+              res.setHeader("Content-Type", "application/json")
+
+              res.end(JSON.stringify(newPet))
+              console.log('ALL GOOD')
             })
           }
         })
       })
+    }
+    else {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'text/plain')
+      res.end(`Not Found`)
+      console.log("HERE WE ARE LOST AGAIN");
     }
   })
 
