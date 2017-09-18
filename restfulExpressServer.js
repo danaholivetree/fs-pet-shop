9c164420812133776e85f868148ae8f863645210
danah olivetree <danah.olivetree@gmail.com>
2017-09-18 12:08:46 -0600

    restful tests pass except status codes dont include headers

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,26 +1,42 @@
-var express = require('express');
-//var router = express.Router();
-var app = express()
 
-var fs = require('fs')
-var path = require('path')
-var url = require('url')
-var port = 8000
-var bodyParser = require('body-parser')
+let fs = require('fs')
+const path = require('path')
+
+const express = require('express');
+const app = express()
+
+const bodyParser = require('body-parser')
+const morgan = require('morgan');
+
 app.use(bodyParser.json())
 app.disable('x-powered-by')
-var file = 'pets.json'
-var filePath = path.join(__dirname, file)
+app.use(morgan('short'));
+
+let port = 8000
+let file = 'pets.json'
+let filePath = path.join(__dirname, file)
 
 app.get('/pets', (req, res) => {
   fs.readFile(filePath, 'utf8', (err, petsJSON) => {
-    res.send(petsJSON)
+    res.send(JSON.parse(petsJSON))
   })
 })
-app.get('/:id', (req, res, next) => {
-
-  console.log("the requested id is", req.params.id);
-  res.send('R in CRUD');
+app.get('/pets/:id', (req, res, next) => {
+  fs.readFile(filePath, 'utf8', (err, petsJSON) => {
+    let pets = JSON.parse(petsJSON)
+    let id = req.params.id
+    if (id < 0 || Number.isNaN(id)) {
+      return res.status(400).end('Bad Request')
+    }
+    else if (id > pets.length) {
+      return res.status(404).end('Not Found')
+    }
+    else {
+      let pet = pets[id]
+      console.log(pets[id])
+      res.send(pet)
+    }
+  })
 });
 
 app.post('/pets', function(req, res, next) {
@@ -28,7 +44,7 @@ app.post('/pets', function(req, res, next) {
   let pet = {name, age, kind}
   pet.age = Number(pet.age)
   if (Number.isNaN(pet.age) || !pet.kind || !pet.name) {
-    res.status(400).end('Bad Request')
+    return res.status(400).end('Bad Request')
   }
   fs.readFile(filePath, 'utf8', (err,petsJSON) => {
     let pets = JSON.parse(petsJSON)
@@ -40,30 +56,35 @@ app.post('/pets', function(req, res, next) {
   })
 })
 
-app.put('/pets/:id', function(req, res, next) {
+app.patch('/pets/:id', function(req, res, next) {
   fs.readFile(filePath, 'utf8', (err, petsJSON) => {
     if (err) {
       throw err
     }
+
     let pets = JSON.parse(petsJSON)
     let id = req.params.id
     if (!id || id < 0 || id > pets.length) {
-      res.status(404).end('Not Found')
+      return res.sendStatus(404)
     }
-    let name = req.body.name
+
     let age = Number(req.body.age)
-    let kind = req.body.kind
-    if (Number.isNaN(req.body.age) || !req.body.kind || !req.body.name) {
-      res.status(400).end('Bad Request')
+    if (!Number.isNaN(age)) {
+      pets[id].age = age
     }
-    let pet = {age, kind, name}
-    let deleted = pets.splice(id, 1, pet)
+    if (req.body.name) {
+      pets[id].name = req.body.name
+    }
+    if (req.body.kind) {
+      pets[id].kind = req.body.kind
+    }
     let newFile = JSON.stringify(pets)
+
     fs.writeFile(filePath, newFile, (err) => {
       if (err) {
         throw error
       }
-      res.send(JSON.stringify(deleted[0]))
+      res.send(pets[id])
     })
   })
 })
@@ -76,21 +97,30 @@ app.delete('/pets/:id', function(req, res, next) {
     let pets = JSON.parse(petsJSON)
     let id = req.params.id
     if (!id || id < 0 || id > pets.length) {
-      res.status(404).end('Not Found')
+      return res.sendStatus(404)
     }
-    let deleted = pets.splice(id, 1)
+    let pet = pets.splice(id, 1)[0]
     let newFile = JSON.stringify(pets)
+
     fs.writeFile(filePath, newFile, (err) => {
       if (err) {
         throw error
       }
-      res.send(JSON.stringify(deleted[0]))
+      res.send(pet)
     })
   })
 })
 
+app.get('/*', (req, res, next) => {
+  res.sendStatus(404)
+})
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
 app.listen(port, () => {
-  console.log('listening')
+  console.log('listening on port', port)
 })
 
 module.exports = app
